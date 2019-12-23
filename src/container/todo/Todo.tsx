7ebe571a1448e3e5738d3b099dd4ebb48e879c77
wenxin.zhang <wenxin.zhang@mobilemd.cn>
2019-12-23 15:00:59 +0800

    todo

--- src/container/todo/Todo.tsx
+++ src/container/todo/Todo.tsx
@@ -1,47 +1,47 @@
-import React, { useState, ReactNode, useContext, Dispatch } from "react";
-import { Tabs, Button, Input, message } from "antd";
+import React, { useState, ReactNode } from "react";
+import { Tabs, } from "antd";
 const { TabPane } = Tabs;
 import "./Todo.css";
 import Unfinish from "./unfinish";
 import Finish from "./finish";
 interface ITodo {
-  id: number;
-  body: string;
-  completed: boolean;
+    id: number;
+    body: string;
+    completed: boolean;
 }
 type ITodoList = ITodo[];
 
 interface IInjected {
-  todoList: ITodoList;
-  setTodoList: (e: Array<any>) => void;
+    todoList: ITodoList;
+    setTodoList: (e: Array<any>) => void;
 }
 export const context = React.createContext<IInjected>({} as IInjected);
 interface IProps {
-  children?: ReactNode;
+    children?: ReactNode;
 }
 function CounterProvider({ children }: IProps) {
-  const [todoList, setCounter] = useState([]);
-  const value = {
-    todoList,
-    setTodoList: (e: any) => setCounter(e)
-  };
-  return <context.Provider value={value}>{children}</context.Provider>;
+    const [todoList, setCounter] = useState([]);
+    const value = {
+        todoList,
+        setTodoList: (e: any) => setCounter(e)
+    };
+    return <context.Provider value={value}>{children}</context.Provider>;
 }
 
 const App: React.FC = props => {
-  return (
-    <Tabs defaultActiveKey="1">
-      <TabPane key="1" tab="未完成">
-        <Unfinish />
-      </TabPane>
-      <TabPane key="2" tab="已完成">
-        <Finish />
-      </TabPane>
-    </Tabs>
-  );
+    return (
+        <Tabs defaultActiveKey="1">
+            <TabPane key="1" tab="未完成">
+                <Unfinish />
+            </TabPane>
+            <TabPane key="2" tab="已完成">
+                <Finish />
+            </TabPane>
+        </Tabs>
+    );
 };
 export default () => (
-  <CounterProvider>
-    <App />
-  </CounterProvider>
+    <CounterProvider>
+        <App />
+    </CounterProvider>
 );
--- typescript/advance.ts
+++ typescript/advance.ts
@@ -16,7 +16,7 @@ function fetchApi<T extends keyof IApiSourceParams>(
   });
 }
 fetchApi("GetList", { id: 2 });
-fetchApi("GetList", { id: "33" }); // 报错, id 应该是 number 类型
+// fetchApi("GetList", { id: "33" }); // 报错, id 应该是 number 类型
 
 // 索引类型
 // 索引类型查询操作符: keyof , 对于任何类型 T， keyof T的结果为 T上已知的公共属性名的联合. 看着话有点绕, 直接看例子吧
