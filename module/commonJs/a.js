6a02aa901133e9448a27f17a66f5ed031a920e07
zhangwenxin <17830723072@dingtalk.com>
2020-12-08 19:28:31 +0800

    a

--- module/commonJs/a.js
+++ module/commonJs/a.js
@@ -1,5 +1,10 @@
 // https://segmentfault.com/a/1190000017878394
 // https://juejin.cn/post/6844904080955932680#heading-8
+// https://segmentfault.com/a/1190000015337690
+// CommonJS定义的模块分为:
 
-// 模块导出 
-export * from 'module'; //重定向导出 不包括 module内的default
+/**
+ 模块引用(require)
+ 模块定义(exports)
+ 模块标识(module)
+ */
